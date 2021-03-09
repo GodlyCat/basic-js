705541f176b3d354660efabef29c004ccae8ef39
godlycat <ufufhby74@gmail.com>
2021-03-09 19:45:46 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,65 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(reverse) {
+    if(reverse===false) this.reverse = false;
+    else this.reverse=true 
+  }
+
+  encrypt(str, key) {
+    if(key===undefined || str===undefined) throw new Error()
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    let localStr = str.toUpperCase().split("")
+    let key2 = key.toUpperCase()
+    let localKey = []
+    var i = 0
+    localStr = localStr.map((item)=>{
+      if(i>key.length-1) i=0
+      if( alphabet.indexOf(item)===-1) {
+        localKey.push(false)
+        return item
+      }
+      else {
+        localKey.push(alphabet.indexOf(key2[i]));
+        i++;
+        return alphabet.indexOf(item)
+      }
+    })
+    localStr = localStr.map((item,index)=>{
+      if(typeof(item)==="number"){
+       return alphabet[(item+localKey[index]) % alphabet.length]
+      }
+      else return item
+    })
+    return this.reverse ? localStr.join(""):localStr.reverse().join("");
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    if(key===undefined || str===undefined) throw new Error()
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+    let localStr = str.toUpperCase().split("")
+    let key2 = key.toUpperCase();
+    let localKey = []
+    var i = 0;
+    localStr = localStr.map((item)=>{
+      if(i>key.length-1) i=0
+      if( alphabet.indexOf(item)===-1) {
+        localKey.push(false)
+        return item
+      }
+      else {
+        localKey.push(alphabet.indexOf(key2[i]));
+        i++;
+        return alphabet.indexOf(item)
+      }
+    })
+    localStr = localStr.map((item,index)=>{
+      if(typeof(item)==="number"){
+       return alphabet[(item+ alphabet.length -localKey[index]) % alphabet.length]
+      }
+      else return item
+    })
+    return this.reverse ? localStr.join(""):localStr.reverse().join("");
   }
 }
 
