9a231947f71ed4f98f0995e9d8b50d9ed28d0ef1
godlycat <ufufhby74@gmail.com>
2021-03-09 17:52:42 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(array) {
+  let comands = ["--discard-prev","--double-prev","--double-next","--discard-next"]
+  if (!Array.isArray(array)) throw new Error()
+  let arr = [...array];
+  let bufferArray = []
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i]===undefined) continue;
+    if( arr[i]==="--discard-prev" && arr[i - 1] !== undefined && !comands.includes(arr[i - 1])) bufferArray.pop();
+    else if( arr[i]==="--double-prev" && (arr[i - 1] !== undefined && !comands.includes(arr[i - 1]))) bufferArray.push(arr[i-1]); 
+    else if( arr[i]==="--double-next" && (arr[i + 1] !== undefined && !comands.includes(arr[i - 1]))) bufferArray.push(arr[i+1]); 
+    else if( arr[i]=== "--discard-next"&& (arr[i + 1] !== undefined && !comands.includes(arr[i - 1]))) arr[i+1]=undefined; 
+    else if(!comands.includes(arr[i])) bufferArray.push(arr[i])
+  }
+  return bufferArray;
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
 };
